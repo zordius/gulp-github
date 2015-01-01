f62d4cc3677179982481d927e07cde1beed481f3
Zordius Chen <zordius@yahoo-inc.com>
2015-01-01 22:17:40 +0800

    Add license, test

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    github = require('./index');
+
+gulp.task('default', function () {
+    return gulp.src('*.js')
+    .pipe(jshint())
+    .pipe(github(options));
+});
