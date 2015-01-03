48a068f6c5c0813cdf2b581958ad4092933a764a
Zordius Chen <zordius@yahoo-inc.com>
2015-01-03 14:52:29 +0800

    export commentToGithub()

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ var through = require('through2'),
 
 simple_reporter = function (E) {
     return ' 1. ' + path.relative(process.cwd(), E.file) + ': line ' + E.error.line + ', col ' + E.error.character + ' *' + E.error.reason + '*';
+},
+
+commentToGithub = function (body, opt) {
+    var GIT = new github(opt.git_option || {
+        version: '3.0.0',
+        headers: {
+            'user-agent': 'gulp-github'
+        }
+    });
+    GIT.authenticate({
+        type: 'oauth',
+        token: opt.git_token
+    });
+
+    GIT.issues.createComment({
+        user: opt.git_repo.split('/')[0],
+        repo: opt.git_repo.split('/')[1],
+        number: opt.git_prid,
+        body: body
+    });
 };
 
 module.exports = function (options) {
@@ -23,30 +43,13 @@ module.exports = function (options) {
         this.push(file);
         callback();
     }, function (cb) {
-        var GIT = new github(opt.git_option || {
-            version: '3.0.0',
-            headers: {
-                'user-agent': 'gulp-github'
-            }
-        });
 
         if (output.length == 1) {
             return cb();
         }
 
         if (opt.git_token && opt.git_repo && opt.git_prid) {
-            GIT.authenticate({
-                type: 'oauth',
-                token: opt.git_token
-            });
-
-            GIT.issues.createComment({
-                user: opt.git_repo.split('/')[0],
-                repo: opt.git_repo.split('/')[1],
-                number: opt.git_prid,
-                body: output.join('\n'),
-            });
-
+            commentToGithub(output.join('\n'), opt);
             gutil.log('[gulp-github]', gutil.colors.bold((output.length - 1) + ' jshint issues were updated to https://' + ((opt.git_option && opt.git_option.host) ? opt.git_option.host : 'github.com') + '/' + opt.git_repo + '/pull/' + opt.git_prid));
         } else {
             console.log('Not a pullrequest or no opts.git_token/opts.git_repo/opts.git_prid');
@@ -58,3 +61,5 @@ module.exports = function (options) {
         cb();
     });
 };
+
+module.exports.commentToGithub = commentToGithub;
