4cd1dde65717e6dd27e597f6615c8f58eb32b173
Chien-hung Chen <zordius@yahoo-inc.com>
2015-01-05 10:51:43 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,11 @@ gulp.task('default', function () {
         // Then save into your .travis.yml
         git_token: process.env.GHTK,
         git_repo: process.env.TRAVIS_REPO_SLUG,
-        git_prid: process.env.TRAVIS_PULL_REQUEST
+        git_prid: process.env.TRAVIS_PULL_REQUEST,
+        git_sha: process.env.TRAVIS_COMMIT,
+
+        jshint_status: 'error',
+        jscs_status: 'failure',
     }))
     .pipe(jshint.reporter('fail'));
 });
